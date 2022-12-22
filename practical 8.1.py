14d2424275ae91bb272f2d7728f610d32ef1d2e7
karti16002 <118901240+karti16002@users.noreply.github.com>
2022-12-22 17:05:35 +0530

    Add files via upload

---
+++ practical 8.1.py
@@ -0,0 +1,10 @@
+b = input("enter a character ")
+if b>='a'and b<='z':
+    print(b,"is a alphabet")
+elif b>='A' and b<='Z':
+    print(b,"is a alphabet")
+elif b>='0' and b<='9':
+    print(b,"is a number")
+else:
+    print(b,"special character")
+
