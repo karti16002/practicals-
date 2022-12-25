4c62547eac25334aa52886a5d17162548af3467f
karti16002 <118901240+karti16002@users.noreply.github.com>
2022-12-25 15:32:31 +0530

    Add files via upload

---
+++ practical 10.py
@@ -0,0 +1,10 @@
+str1 = input("Please Enter First String : ")
+str2 = input("Please Enter Second String : ")
+n = int(input("Enter no. of chars to swap : "))
+x=str1[0:n]
+
+str1=str1.replace(str1[0:n],str2[0:n])
+str2=str2.replace(str2[0:n],x)
+
+print("your first string is:-"+ str1)
+print("your second string is:-"+ str2)
