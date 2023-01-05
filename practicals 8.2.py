50895e14212876da3a5f34af032a0de04b899916
karti16002 <118901240+karti16002@users.noreply.github.com>
2023-01-05 16:47:31 +0530

    Add files via upload

---
+++ practicals 8.2.py
@@ -0,0 +1,8 @@
+b = input("Enter a character:- ")
+if b>='a'and b<='z':
+    print(b,"is a lowercase alphabet")
+elif b>='A' and b<='Z':
+    print(b,"is a uppercase alphabet")
+else:
+    print("Enter character is not a alphabet ")
+
